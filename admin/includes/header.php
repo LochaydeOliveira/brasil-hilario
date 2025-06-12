2052d760123f016b066519f3bb611af6971de88c
unknown <lochaydeguerreiro@hotmail.com>
2025-06-12 19:56:23 -0300

    Atualizei o Header

--- admin/includes/header.php
+++ admin/includes/header.php
@@ -150,19 +150,19 @@ if (!isLoggedIn()) {
 <body>
     <header class="navbar sticky-top flex-md-nowrap p-0 shadow">
         <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="index.php">
-            <img src="../assets/img/logo-brasil-hilario-quadrada-svg.svg" alt="Brasil Hilário" class="img-fluid">
+            <img width="150p" src="../assets/img/logo-brasil-hilario-quadrada-svg.svg" alt="Brasil Hilário" class="img-fluid">
         </a>
         <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="w-100"></div>
-        <div class="navbar-nav">
+        <!-- <div class="navbar-nav">
             <div class="nav-item text-nowrap">
                 <a class="nav-link px-3" href="logout.php">
                     <i class="fas fa-sign-out-alt"></i> Sair
                 </a>
             </div>
-        </div>
+        </div> -->
     </header>
 
     <div class="container-fluid">
@@ -173,13 +173,13 @@ if (!isLoggedIn()) {
                 </div>
             </nav>
 
-            <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+            <!-- <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
                 <?php if (isset($page_title)): ?>
                 <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
                     <h1 class="h2"><?php echo $page_title; ?></h1>
                 </div>
                 <?php endif; ?>
-            </main>
+            </main> -->
         </div>
     </div>
 </body>
