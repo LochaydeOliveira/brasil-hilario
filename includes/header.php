9c5258378eb82a129a59f0d9044fdbb9377b99fd
unknown <lochaydeguerreiro@hotmail.com>
2025-07-22 22:48:11 -0300

    Defini Variavel Adsense 4

--- includes/header.php
+++ includes/header.php
@@ -7,9 +7,6 @@ require_once 'config/config.php';
     $page_url = isset($og_url) ? $og_url : BLOG_URL;
     $page_image = isset($og_image) ? $og_image : BLOG_URL . '/assets/img/logo-brasil-hilario-para-og.png';
     $page_og_type = isset($og_type) ? $og_type : 'website';
-    $adsense_client_id = defined('ADSENSE_CLIENT_ID') ? ADSENSE_CLIENT_ID : '';
-    $adsense_slot_id = defined('ADSENSE_SLOT_ID') ? ADSENSE_SLOT_ID : '';
-
 
 
     $categories = [];
@@ -84,7 +81,7 @@ require_once 'config/config.php';
     <title><?php echo $page_title; ?></title>
     <meta name="description" content="<?php echo $page_description; ?>">
     <meta name="keywords" content="<?php echo $page_keywords; ?>">
-    <meta name="google-adsense-account" content="ca-pub-<?php echo $adsense_client_id; ?>">
+    <meta name="google-adsense-account" content="ca-pub-8313157699231074">
 
     <meta property="og:type" content="<?php echo $page_og_type; ?>">
     <meta property="og:url" content="<?php echo $page_url; ?>">
