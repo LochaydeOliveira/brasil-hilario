8b4175dac2191a038f4867a7743e647af30b5b49
unknown <lochaydeguerreiro@hotmail.com>
2025-07-23 02:53:23 -0300

    Correção DB PDO

--- includes/db.php
+++ includes/db.php
@@ -5,15 +5,9 @@ $user = "paymen58";
 $pass = "u4q7+B6ly)obP_gxN9sNe";
 
 try {
-    $conn = new mysqli($host, $user, $pass, $db);
-    
-    if ($conn->connect_error) {
-        throw new Exception("Erro na conexão: " . $conn->connect_error);
-    }
-    
-    $conn->set_charset("utf8mb4");
-    
-} catch (Exception $e) {
+    $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8mb4", $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
     error_log("Erro na conexão com o banco de dados: " . $e->getMessage());
     die("Erro na conexão com o banco de dados. Por favor, tente novamente mais tarde.");
-} 
+}
