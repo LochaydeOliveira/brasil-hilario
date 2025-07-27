1ba1cf7ed7301cdf63b88ed429283505237e88db
unknown <lochaydeguerreiro@hotmail.com>
2025-07-26 22:15:27 -0300

    Nav

--- includes/header.php
+++ includes/header.php
@@ -104,7 +104,7 @@ try {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
-    <link href="<?php echo BLOG_URL; ?>/assets/css/style.css?v=03" rel="stylesheet">
+    <link href="<?php echo BLOG_URL; ?>/assets/css/style.css?v=04" rel="stylesheet">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 
