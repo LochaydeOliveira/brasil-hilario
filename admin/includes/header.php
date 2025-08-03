4d048e67e16fe19844ccd8d6e5caa2ffd36e8581
unknown <lochaydeguerreiro@hotmail.com>
2025-08-02 23:58:29 -0300

    Fonte do Admin

--- admin/includes/header.php
+++ admin/includes/header.php
@@ -279,4 +279,4 @@ if (!isset($_SESSION['usuario_id'])) {
                 </div>
             </nav>
 
-            <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+            <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4 sz-fnt">
